8f407e83c72e39710660ea84aa006d71aa9e9bac
prashantpiprotar <prashplus@gmail.com>
2018-07-25 22:53:40 +0530

    Linear regression init added and data file

--- linear regression/linear_regression_init.py
+++ linear regression/linear_regression_init.py
@@ -1,17 +1,48 @@
-import pandas as pd
-from sklearn import linear_model
-import matplotlib.pyplot as plt
+#This is just to demostrate gradient descent
 
-#read data
-dataframe = pd.read_fwf('brain_body.txt')
-x_values = dataframe[['Brain']]
-y_values = dataframe[['Body']]
+from numpy import *
 
-#train model on data
-body_reg = linear_model.LinearRegression()
-body_reg.fit(x_values, y_values)
+# using the general linear equation y=mx + c
 
-#visualize results
-plt.scatter(x_values,y_values)
-plt.plot(x_values,body_reg.predict(x_values))
-plt.show()
+def compute_error (b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i,0]
+        y = points[i,1]
+        totalError += (y-(m*x + b)) ** 2
+    return totalError / float(len(points))
+
+
+def step_gradient(b_current, m_current, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i,0]
+        y = points[i,1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)
+    new_m = m_current - (learningRate * m_gradient)
+    return [new_b, new_m]
+
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+    b = starting_b
+    m = starting_m
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, array(points), learning_rate)
+    return [b, m]
+
+def run():
+    points = genfromtxt("data.csv", delimiter=",")
+    learning_rate = 0.0001
+    initial_b = 0 # initial y-intercept guess
+    initial_m = 0 # initial slope guess
+    num_iterations = 1000
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
+    print("Running...")
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations))
+    #print("After {0} iterations b = {1}, m = {2}, error = {3}").format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
+
+if __name__ == '__main__':
+    run()
